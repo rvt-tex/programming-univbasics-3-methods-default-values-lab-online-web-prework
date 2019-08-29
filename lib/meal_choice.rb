e3833f743e843e8bca212e48619d6f38bbc0ff03
Ruel Tingling <rvtingling@yahoo.com>
2019-08-29 14:54:40 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,28 @@
 # Your code here
+
+def meal_type1(veg1)
+  puts "#{veg1}"
+end
+
+def meal_type2(veg2)
+  puts "#{veg2}"
+end
+
+def meal_type3(protein)
+  puts "#{protein}"
+end
+
+
+
+def meal_choice(veg1, veg2, protein = "meat")
+  puts "Whether youre omnivourous, vegetarian, or vegan, your going to be eating a nutrious meal!"
+end
+
+def meal
+puts "#{what a nutrious meal!}"
+end
+
+def selected_meal
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
